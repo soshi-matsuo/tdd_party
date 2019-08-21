500b613de28c5feb208b7aa34f63ec029f4e453e
soshi-matsuo <msoshi1202@gmail.com>
2019-08-21 19:41:59 +0900

    done poker

--- poker.py
+++ poker.py
@@ -13,7 +13,7 @@ def is_flush(hands):
     return True
 
 def is_straight(hands):
-    sorted_hand = sorted([int(hand[0]) for hand in hands])
+    sorted_hand = sorted([str2num(hand[0]) for hand in hands])
 
     j = 0
     while j < len(sorted_hand) -1:
@@ -26,3 +26,81 @@ def is_four_card(hands):
     hands_num = [hand[0] for hand in hands]
     c = Counter(hands_num)
     return max(c.values()) == 4
+
+# def has_same_three(hands):
+#     hands_num = [hand[0] for hand in hands]
+#     c = Counter(hands_num)
+#     return max(c.values()) == 3
+
+def is_three_card(hands):
+    hands_num = [hand[0] for hand in hands]
+    c = Counter(hands_num)
+    result = c.values()
+    return max(result) == 3 and min(result) == 1
+
+def is_full_house(hands):
+    hands_num = [hand[0] for hand in hands]
+    c = Counter(hands_num)
+    result = c.values()
+    return max(result) == 3 and min(result) == 2
+
+def is_one_pair(hands):
+    hands_num = [hand[0] for hand in hands]
+    c = Counter(hands_num)
+    result = c.values()
+
+    return len(result) == 4
+
+def is_two_pair(hands):
+    hands_num = [hand[0] for hand in hands]
+    c = Counter(hands_num)
+    result = c.values()
+    return max(result) == 2 and len(result) == 3
+
+def judge_hands(hands):
+    if is_one_pair(hands):
+        judgement = 1
+    elif is_two_pair(hands):
+        judgement = 2
+    elif is_straight_flush(hands):
+        judgement = 8
+    elif is_three_card(hands):
+        judgement = 3
+    elif is_straight(hands):
+        judgement = 4
+    elif is_flush(hands):
+        judgement = 5
+    elif is_four_card(hands):
+        judgement = 7
+    elif is_full_house(hands):
+        judgement = 6
+    else:
+        judgement = 0
+    
+    return judgement
+
+def str2num(char):
+    if char is 'A':
+        return 1
+    elif char is 'T':
+        return 10
+    elif char is 'J':
+        return 11
+    elif char is 'Q':
+        return 12
+    elif char is 'K':
+        return 13
+    else:
+        return int(char)
+
+def fuck(player1, player2):
+    player1_score = judge_hands(player1)
+    player2_score = judge_hands(player2)
+
+    if player1_score > player2_score:
+        return "PLAYER1 WIN!!"
+    elif player1_score == player2_score:
+        return "DRAW"
+    else:
+        return "PLAYER2 WIN!!"
+
--- test_poker.py
+++ test_poker.py
@@ -5,20 +5,28 @@ class TestPoker(unittest.TestCase):
 
     def setUp(self):
         self.hands_patterns = [
-            ['4H', '3H', '2H', '5H', '6H'],
-            ['2H', "3H", '4H', '5H', '6D'],
-            ['2H', "3H", '5H', '6H', '7H'],
-            ['2H', "3H", '6S', '5H', '7D'],
-            ['2H', "2D", '2S', '2C', '7D']
+            ['4H', '3H', '2H', '5H', '6H'], # straight flush
+            ['2H', "3H", '4H', '5H', '6D'], # straight
+            ['2H', "3H", '5H', '6H', '7H'], # flush
+            ['2H', "3H", '6S', '5H', '7D'], # high card
+            ['2H', "2D", '2S', '2C', '7D'], # four card
+            ['3D', '3H', '3S', '5H', '6H'], # three card
+            ['3D', '3H', '3S', '6S', '6H'], # full house
+            ['5H', '5S', '4H', '3H', '9H'], # one pair
+            ['5H', '5S', '3S', '3H', '9H'], # two pair
+            ['4H', '3H', '2H', '5H', 'AH'], # straight flush
+            ['9H', "TH", 'JH', 'QH', 'KD'], # straight
         ]
+
     @unittest.SkipTest
     def test_is_high_card(self):
+        label = True
         hands = ['2H', '4H', '5C', '9S', 'AS']
-        self.assertEqual(True, True)
+        self.assertEqual(poker.is_high_card(hands), label)
 
     def test_is_straight_flush(self):
         label_patterns = [
-            True, False, False, False, False
+            True, False, False, False, False, False, False, False, False, True, False
         ]
         for hands, label in zip(self.hands_patterns, label_patterns):
             with self.subTest(hands=hands, label=label):
@@ -26,7 +34,7 @@ class TestPoker(unittest.TestCase):
 
     def test_is_flush(self):
         label_patterns = [
-            True, False, True, False, False
+            True, False, True, False, False, False, False, False, False, True, False
         ]
         for hands, label in zip(self.hands_patterns, label_patterns):
             with self.subTest(hands=hands, label=label):
@@ -35,7 +43,7 @@ class TestPoker(unittest.TestCase):
 
     def test_is_straight(self):
         label_patterns = [
-            True, True, False, False, False
+            True, True, False, False, False, False, False, False, False, True, True
         ]
         for hands, label in zip(self.hands_patterns, label_patterns):
             with self.subTest(hands=hands, label=label):
@@ -43,11 +51,70 @@ class TestPoker(unittest.TestCase):
 
     def test_is_four_card(self):
         label_patterns = [
-            False, False, False, False, True
+            False, False, False, False, True, False, False, False, False, False, False
         ]
         for hands, label in zip(self.hands_patterns, label_patterns):
             with self.subTest(hands=hands, label=label):
                 self.assertEqual(poker.is_four_card(hands), label)
+
+    def test_is_three_card(self):
+        label_patterns = [
+            False, False, False, False, False, True, False, False, False, False, False
+        ]
+        for hands, label in zip(self.hands_patterns, label_patterns):
+            with self.subTest(hands=hands, label=label):
+                self.assertEqual(poker.is_three_card(hands), label)
+
+    def test_is_full_house(self):
+        label_patterns = [
+            False, False, False, False, False, False, True, False, False, False, False
+        ]
+        for hands, label in zip(self.hands_patterns, label_patterns):
+            with self.subTest(hands=hands, label=label):
+                self.assertEqual(poker.is_full_house(hands), label)
+    
+    def test_is_one_pair(self):
+        label_patterns = [
+            False, False, False, False, False, False, False, True, False, False, False
+        ]
+        for hands, label in zip(self.hands_patterns, label_patterns):
+            with self.subTest(hands=hands, label=label):
+                self.assertEqual(poker.is_one_pair(hands), label)
+
+    def test_is_two_pair(self):
+        label_patterns = [
+            False, False, False, False, False, False, False, False, True, False, False
+        ]
+        for hands, label in zip(self.hands_patterns, label_patterns):
+            with self.subTest(hands=hands, label=label):
+                self.assertEqual(poker.is_two_pair(hands), label)
+
+    def test_judge_hands(self):
+        label_patterns = [8, 4, 5, 0, 7, 3, 6, 1, 2, 8, 4]
+
+        for hands, label in zip(self.hands_patterns, label_patterns):
+            with self.subTest(hands=hands, label=label):
+                self.assertEqual(poker.judge_hands(hands), label)
+
+    def test_str2num(self):
+        test = 'A'
+        self.assertEqual(poker.str2num(test), 1)
+        test = 'T'
+        self.assertEqual(poker.str2num(test), 10)
+        test = 'J'
+        self.assertEqual(poker.str2num(test), 11)
+        test = 'Q'
+        self.assertEqual(poker.str2num(test), 12)
+        test = 'K'
+        self.assertEqual(poker.str2num(test), 13)
+    
+    def test_fuck(self):
+        player1 = ['2H', '4H', '5C', '9S', 'AS']
+        player2 = ['2H', '4H', '2C', '9S', 'AS']
+
+        self.assertEqual(poker.fuck(player1, player2), "PLAYER2 WIN!!")
+
+        
 
 if __name__ == '__main__':
     unittest.main()
